a3a73625f1508ff837c31d3d0cc0dbea15bc8997
Jelle De Loecker <jelle@codedor.be>
2014-10-12 13:47:34 +0200

    test: JSON.dry & .undry unit tests

--- test/json.js
+++ test/json.js
@@ -3,6 +3,9 @@ var assert = require('assert'),
 
 describe('JSON', function() {
 
+	var dryCirc2,
+	    dryCirc;
+
 	before(function() {
 		Blast  = require('../index.js')();
 	});
@@ -10,6 +13,138 @@ describe('JSON', function() {
 	describe('#toSource()', function() {
 		it('should return the source code representation of the JSON object', function() {
 			assert.equal('JSON', JSON.toSource());
+		});
+	});
+
+	describe('.dry()', function() {
+		it('should stringify the object', function() {
+
+			var obj = {},
+			    arr = [0,1,2],
+			    undry,
+			    json,
+			    dry;
+
+			obj.a = arr;
+			obj.b = arr;
+
+			json = JSON.stringify(obj);
+			dry = JSON.dry(obj);
+
+			assert.equal(true, dry.length < json.length, 'Dry string is not shorter than JSON string');
+
+			undry = JSON.undry(dry);
+
+			assert.equal(undry.a, undry.b, 'Array a & b should be references to the same array');
+		});
+
+		it('should stringify circular references', function() {
+
+			var obj = {test: true, arr: [0,1,2]},
+			    dry;
+
+			obj.circle = obj;
+
+			dry = JSON.dry(obj);
+			dryCirc = dry;
+
+			assert.equal('{"test":true,"arr":[0,1,2],"circle":"~"}', dry);
+
+			obj.deep = {obj: obj};
+			dryCirc2 = JSON.dry(obj);
+		});
+
+		it('should use #toDry() method of objects', function() {
+
+			var d = new Deck(),
+			    ntemp,
+			    temp,
+			    ndry,
+			    obj,
+			    dry;
+
+			d.push('first');
+			obj = {nonroot: d};
+
+			dry = JSON.dry(d);
+			ndry = JSON.dry(obj);
+			temp = JSON.parse(dry);
+			ntemp = JSON.parse(ndry);
+
+			assert.equal('__Protoblast.Classes.Deck', temp.path);
+			assert.equal('__Protoblast.Classes.Deck', ntemp.nonroot.path);
+		});
+	});
+
+	describe('.undry()', function() {
+
+		it('should parse circular references', function() {
+
+			var undry2,
+			    undry;
+
+			undry = JSON.undry(dryCirc);
+
+			assert.equal(true, undry === undry.circle, 'Circular reference in undried object is gone');
+
+			undry2 = JSON.undry(dryCirc2);
+
+			assert.equal(true, undry2 === undry2.deep.obj, 'Deep circular reference in undried object is gone');
+		});
+
+		it('should use undry on dried objects that need to be revived', function() {
+
+			var d = new Deck(),
+			    dry,
+			    undry;
+
+			d.push('first');
+			dry = JSON.dry({mydeck: d});
+
+			undry = JSON.undry(dry);
+
+			assert.equal(true, undry.mydeck instanceof Deck);
+		});
+
+		it('should also be able to revive root objects', function() {
+
+			var d = new Deck(),
+			    dry,
+			    undry;
+
+			d.push('first');
+			dry = JSON.dry(d);
+
+			undry = JSON.undry(dry);
+
+			assert.equal(true, undry instanceof Deck);
+		});
+
+		it('should handle complicated revive structures', function() {
+
+			var d = new Deck(),
+			    d2 = new Deck(),
+			    arr,
+			    dry,
+			    sub,
+			    undry;
+
+			arr = [0,1,2];
+			d2.push('sub');
+			d2.set('arr', arr);
+
+			d.push('first');
+			d.set('subdeck', d2);
+			d.set('arr', arr);
+
+			dry = JSON.dry(d);
+
+			undry = JSON.undry(dry);
+			sub = undry.get('subdeck');
+
+			assert.equal(true, undry instanceof Deck);
+			assert.equal(true, sub instanceof Deck);
+			assert.equal(true, undry.get('arr') === sub.get('arr'));
 		});
 	});
 
