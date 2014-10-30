2e6cea51841fced43d0cb3a2107b7196ff0a9afc
Jelle De Loecker <jelle@codedor.be>
2014-10-30 16:10:41 +0100

    test: error unit tests (back at 100% coverage)

--- test/error.js
+++ test/error.js
@@ -14,7 +14,36 @@ describe('Error', function() {
 			    begin = '(new Error("msg", ';
 
 			assert.equal(begin, e.toSource().slice(0,18));
+
+			var line = new Error('msg');
+			line.lineNumber = 0;
+			src = line.toSource();
+
+			assert.equal('(new Error("msg", undefined))', src);
+
+			line = new Error('msg');
+			line.lineNumber = null;
+			src = line.toSource();
+
+			assert.equal('(new Error("msg", undefined))', src);
+
+			line = new Error('msg');
+			line.lineNumber = 10;
+			src = line.toSource();
+
+			assert.equal('(new Error("msg", undefined, 10))', src);
 		});
 	});
 
+	describe('.unDry() & #toDry()', function() {
+		it('should be able to stringify & revive an error', function() {
+
+			var e = new Error('msg'),
+			    str = JSON.dry(e),
+			    revive = JSON.undry(str);
+
+			assert.equal(e.message, revive.message, 'Message is not equal');
+			assert.equal(e.stack, revive.stack, 'Stack is not equal');
+		});
+	});
 });
