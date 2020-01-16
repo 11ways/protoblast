11ee3269d2520f99b476b7159ed6a7c817a4c5df
Jelle De Loecker <jelle@develry.be>
2020-01-16 13:44:46 +0100

    Test: Add new underscore test + fix assert order

--- test/inflections.js
+++ test/inflections.js
@@ -9,131 +9,140 @@ describe('String Inflections', function() {
 
 	describe('#pluralize()', function() {
 		it('should return the plural representation of the string', function() {
-			assert.strictEqual('buses', 'bus'.pluralize());
-			assert.strictEqual('windows', 'window'.pluralize());
-			assert.strictEqual('octopi', 'octopus'.pluralize());
+			assert.strictEqual('bus'.pluralize(), 'buses');
+			assert.strictEqual('window'.pluralize(), 'windows');
+			assert.strictEqual('octopus'.pluralize(), 'octopi');
 		});
 	});
 
 	describe('#singularize()', function() {
 		it('should return the singular representation of the string', function() {
-			assert.strictEqual('bus', 'buses'.singularize());
-			assert.strictEqual('window', 'windows'.singularize());
-			assert.strictEqual('octopus', 'octopi'.singularize());
+			assert.strictEqual('buses'.singularize(), 'bus');
+			assert.strictEqual('windows'.singularize(), 'window');
+			assert.strictEqual('octopi'.singularize(), 'octopus');
 		});
 	});
 
 	describe('#modelName(postfix)', function() {
 		it('should return the model name representation of the string', function() {
-			assert.strictEqual('News', 'news'.modelName());
-			assert.strictEqual('User', 'user'.modelName());
-			assert.strictEqual('App', 'app'.modelName());
+			assert.strictEqual('news'.modelName(), 'News');
+			assert.strictEqual('user'.modelName(), 'User');
+			assert.strictEqual('app'.modelName(), 'App');
 		});
 	});
 
 	describe('#modelClassName()', function() {
 		it('should return the model name representation of the string, with the Model postfix', function() {
-			assert.strictEqual('NewsModel', 'news'.modelClassName());
-			assert.strictEqual('NewsModel', 'newsModel'.modelClassName());
-			assert.strictEqual('NewsModel', 'news_model'.modelClassName());
-			assert.strictEqual('UserModel', 'user'.modelClassName());
-			assert.strictEqual('AppModel', 'app'.modelClassName());
+			assert.strictEqual('news'.modelClassName(), 'NewsModel');
+			assert.strictEqual('newsModel'.modelClassName(), 'NewsModel');
+			assert.strictEqual('news_model'.modelClassName(), 'NewsModel');
+			assert.strictEqual('user'.modelClassName(), 'UserModel');
+			assert.strictEqual('app'.modelClassName(), 'AppModel');
 		});
 	});
 
 	describe('#controllerName(postfix)', function() {
 		it('should return the controller name representation of the string', function() {
 
-			assert.strictEqual('App', 'app'.controllerName());
-			assert.strictEqual('Static', 'static'.controllerName());
+			assert.strictEqual('app'.controllerName(), 'App');
+			assert.strictEqual('static'.controllerName(), 'Static');
 
-			assert.strictEqual('News', 'news'.controllerName());
-			assert.strictEqual('Users', 'user'.controllerName());
-			assert.strictEqual('ContactInfos', 'contact_info'.controllerName());
+			assert.strictEqual('news'.controllerName(), 'News');
+			assert.strictEqual('user'.controllerName(), 'Users');
+			assert.strictEqual('contact_info'.controllerName(), 'ContactInfos');
 		});
 	});
 
 	describe('#controllerClassName(postfix)', function() {
 		it('should return the controller name representation of the string, with the Controller postfix', function() {
 
-			assert.strictEqual('AppController', 'app'.controllerClassName());
-			assert.strictEqual('StaticController', 'static'.controllerClassName());
+			assert.strictEqual('app'.controllerClassName(), 'AppController');
+			assert.strictEqual('static'.controllerClassName(), 'StaticController');
 
-			assert.strictEqual('NewsController', 'news'.controllerClassName());
-			assert.strictEqual('NewsController', 'newsController'.controllerClassName());
-			assert.strictEqual('NewsController', 'news_controller'.controllerClassName());
-			assert.strictEqual('UsersController', 'user'.controllerClassName());
-			assert.strictEqual('ContactInfosController', 'contact_info'.controllerClassName());
+			assert.strictEqual('news'.controllerClassName(), 'NewsController');
+			assert.strictEqual('newsController'.controllerClassName(), 'NewsController');
+			assert.strictEqual('news_controller'.controllerClassName(), 'NewsController');
+			assert.strictEqual('user'.controllerClassName(), 'UsersController');
+			assert.strictEqual('contact_info'.controllerClassName(), 'ContactInfosController');
 		});
 	});
 
 	describe('#camelize(lowFirstLetter)', function() {
 		it('should return the camelized representation of the string', function() {
-			assert.strictEqual('ThisIsATest', 'this_is_a_test'.camelize());
-			assert.strictEqual('thisIsATest', 'this_is_a_test'.camelize(true));
+			assert.strictEqual('this_is_a_test'.camelize(), 'ThisIsATest');
+			assert.strictEqual('this_is_a_test'.camelize(true), 'thisIsATest');
 		});
 	});
 
 	describe('#underscore()', function() {
 		it('should return the underscored representation of the string', function() {
-			assert.strictEqual('this_is_a_test', 'this is a test'.underscore());
+			assert.strictEqual('this is a test'.underscore(), 'this_is_a_test');
+			assert.strictEqual('the-underscored-string-method'.underscore(), 'the_underscored_string_method');
+			assert.strictEqual('theUnderscoredStringMethod'.underscore(), 'the_underscored_string_method');
+			assert.strictEqual('TheUnderscoredStringMethod'.underscore(), 'the_underscored_string_method');
+			assert.strictEqual(' the underscored  string method'.underscore(), 'the_underscored_string_method');
+			assert.strictEqual('   the underscored  string  method'.underscore(), 'the_underscored_string_method');
+			assert.strictEqual('   the underscored _ string  method'.underscore(), 'the_underscored_string_method');
+			assert.strictEqual(''.underscore(), '');
+			assert.strictEqual('ThisIsATest'.underscore(), 'this_is_a_test');
+			assert.strictEqual(' ThisIsATest'.underscore(), 'this_is_a_test');
 		});
 	});
 
 	describe('#humanize(lowFirstLetter)', function() {
 		it('should return a human readable representation of the string', function() {
-			assert.strictEqual('This should be humanized', 'this should be humanized'.humanize());
-			assert.strictEqual('this should be humanized', 'this should be humanized'.humanize(true));
+			assert.strictEqual('this should be humanized'.humanize(), 'This should be humanized');
+			assert.strictEqual('this should be humanized'.humanize(true), 'this should be humanized');
 		});
 	});
 
 	describe('#capitalize()', function() {
 		it('should return the string with only the first letter being uppercased', function() {
-			assert.strictEqual('This should be capitalized', 'this should be capitalized'.capitalize());
+			assert.strictEqual('this should be capitalized'.capitalize(), 'This should be capitalized');
 		});
 	});
 
 	describe('#dasherize()', function() {
 		it('should return underscores and spaces with strings', function() {
-			assert.strictEqual('dash-erize-this', 'dash_erize this'.dasherize());
+			assert.strictEqual('dash_erize this'.dasherize(), 'dash-erize-this');
 		});
 	});
 
 	describe('#titleize()', function() {
 		it('should return the titleized representation of the string', function() {
-			assert.strictEqual('This Is a Title', 'this is a title'.titleize());
+			assert.strictEqual('this is a title'.titleize(), 'This Is a Title');
 		});
 	});
 
 	describe('#demodulize()', function() {
 		it('should remove module names leaving only class names', function() {
-			assert.strictEqual('Properties', "Message::Bus::Properties".demodulize());
+			assert.strictEqual("Message::Bus::Properties".demodulize(), 'Properties');
 		});
 	});
 
 	describe('#tableize()', function() {
 		it('should return the underscored plural form of the string', function() {
-			assert.strictEqual('my_users', 'MyUser'.tableize());
-			assert.strictEqual('users', 'user'.tableize());
+			assert.strictEqual('MyUser'.tableize(), 'my_users');
+			assert.strictEqual('user'.tableize(), 'users');
 		});
 	});
 
 	describe('#tableize()', function() {
 		it('should return strings into their camel cased singular form', function() {
-			assert.strictEqual('User', 'users'.classify());
+			assert.strictEqual('users'.classify(), 'User');
 		});
 	});
 
 	describe('#foreign_key()', function() {
 		it('should turn a strings into an underscored foreign key', function() {
-			assert.strictEqual('user_id', 'user'.foreign_key());
+			assert.strictEqual('user'.foreign_key(), 'user_id');
 		});
 	});
 
 	describe('#ordinalize()', function() {
 		it('should renders all found numbers their sequence', function() {
-			assert.strictEqual('the 1st pitch', 'the 1 pitch'.ordinalize());
-			assert.strictEqual('the 2nd pitch', 'the 2 pitch'.ordinalize());
+			assert.strictEqual('the 1 pitch'.ordinalize(), 'the 1st pitch');
+			assert.strictEqual('the 2 pitch'.ordinalize(), 'the 2nd pitch');
 		});
 	});
 
