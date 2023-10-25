5ec3ced016a58a852e7b6049a3e4c7b99444029c
Jelle De Loecker <jelle@elevenways.be>
2023-10-25 15:14:06 +0200

    ✨ Add `Decimal#multiply(value)`

--- test/decimal.js
+++ test/decimal.js
@@ -129,8 +129,78 @@ describe('Decimal', function() {
 		});
 	});
 
+	describe('#multiply(value)', () => {
+
+		it('should multiply simple integers', () => {
+			multiplications([
+				[ '0',  '1',  '0'],
+				[ '1',  '1',  '1'],
+				[ '2',  '1',  '2'],
+				[ '2',  '2',  '4'],
+				[ '2',  '3',  '6'],
+				['-1', '-1',  '1'],
+				['-2',  '2', '-4'],
+				['-2', '-2',  '4'],
+			]);
+		});
+
+		it('should multiply simple decimals', () => {
+			multiplications([
+				[  '0.1',    '1',   '0.1'],
+				[  '0.1',    '2',   '0.2'],
+				[  '0.1',    '3',   '0.3'],
+				['-10.0',  '0.1',    '-1'],
+				[  '0.1',  '0.1',  '0.01'],
+				[  '0.1',  '0.2',  '0.02'],
+				[  '0.5',  '0.5',  '0.25'],
+				[  '0.5',    '8',     '4'],
+				[  '0.5',    '9',   '4.5'],
+				[  '0.5',   '10',     '5'],
+				[  '0.5',   '11',   '5.5'],
+				[   '23',   '42',   '966'],
+				[   '23',  '-42',  '-966'],
+			]);
+		});
+
+	});
+
+	
+
 });
 
-function decimalEquals(decimal, str) {
-	assert.strictEqual(decimal.toString(), str);
+function pow(first_string, second_string, result_string) {
+	let first = Decimal(first_string),
+	    second = Decimal(second_string);
+
+	let sum = first.pow(second);
+
+	decimalEquals(sum, result_string, `Decimal('${first_string}').pow('${second_string}') should equal Decimal('${result_string}')`);
+}
+
+function multiplications(arr) {
+
+	for (let entry of arr) {
+		let a = Decimal(entry[0]),
+		    b = Decimal(entry[1]),
+		    string_result = entry[2];
+
+		let c = a.multiply(b);
+
+		let msg = `Decimal('${entry[0]}).multiply('${entry[1]}') should equal Decimal('${entry[2]}')`;
+
+		decimalEquals(c, string_result, msg);
+	}
+}
+
+function addition(first_string, second_string, result_string) {
+	let first = Decimal(first_string),
+	    second = Decimal(second_string);
+
+	let sum = first.add(second);
+
+	decimalEquals(sum, result_string, `Decimal('${first_string}').add('${second_string}') should equal Decimal('${result_string}')`);
+}
+
+function decimalEquals(decimal, str, msg) {
+	assert.strictEqual(decimal.toString(), str, msg);
 }
